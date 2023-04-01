ecb8fe3b2cf67a2aa1a63afeac8f7e52e4b79bb6
asianovel <asianovel001@gmail.com>
2023-04-01 16:49:21 +0800

    fix

--- app/src/main/java/net/asianovel/reader/data/AppDatabase.kt
+++ app/src/main/java/net/asianovel/reader/data/AppDatabase.kt
@@ -43,7 +43,7 @@ val appDb by lazy {
         AutoMigration(from = 55, to = 56),
         AutoMigration(from = 56, to = 57),
         AutoMigration(from = 57, to = 58),
-        AutoMigration(from = 58, to = 59),
+        AutoMigration(from = 58, to = 59)
     ]
 )
 abstract class AppDatabase : RoomDatabase() {
